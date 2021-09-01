b2ba6d6f160ceeebc527a73077daa60863aa6643
arsentiy82 <arssentiyy@yandex.ru>
2021-09-01 22:30:00 +0300

    0109

--- src/main/java/ru/gb/JdbcApp.java
+++ src/main/java/ru/gb/JdbcApp.java
@@ -2,5 +2,43 @@ package ru.gb;
 import java.sql.*;
 
 public class JdbcApp {
+    private Connection connection;
+    private Statement statement;
+
+    public static void main(String[] args) {
+        final JdbcApp JdbcApp = new JdbcApp();
+        try {
+            JdbcApp.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcApp.disconnect();
+        }
+    }
+    private void createTable(){
+
+    }
+    private void disconnect() {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if(connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void connect() throws SQLException {
+        connection=DriverManager.getConnection("jdbc:sqlite:javadb.db");
+        statement = connection.createStatement();
+
+    }
 
 }
